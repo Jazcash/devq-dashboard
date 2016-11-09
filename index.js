a6bf96cce2416a87824de37eb67bacf57cae0f13
jazcash <jaspercashmore@gmail.com>
2016-11-09 15:22:37 +0000

    Basic functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const Donedone = require("donedone.js");
 const config = require("./config");
 
@@ -13,17 +13,23 @@ let donedone = new Donedone({
 let filter = donedone.getGlobalFiltersSync().find((filter) => filter.name === config.filter);
 let issues = donedone.getIssuesByFilterSync(filter.id).issues;
 
-console.log(issues.length);
+setInterval(function(){
+    issues = donedone.getIssuesByFilterSync(filter.id).issues;
+    io.emit("issues", issues);
+}, 5000);
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", function(socket){
-    console.log("a user connected");
-    socket.emit("data", data);
+    console.log("A client connected");
 
+    socket.emit("issues", issues);
     
+    socket.on("disconnect", function(){
+        console.log("A client disconnected");
+    });
 });
 
 http.listen(3000, function(){
