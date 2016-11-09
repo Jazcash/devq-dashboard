2a395341d997c1229abb1c119bce97f49598de85
jazcash <jaspercashmore@gmail.com>
2016-11-09 14:32:54 +0000

    Added basic files

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 const Donedone = require("donedone.js");
 const config = require("./config");
 
@@ -7,6 +10,22 @@ let donedone = new Donedone({
     apikey: config.apikey
 });
 
-donedone.getCompanies(function(companies){
-    console.log(companies);
+let filter = donedone.getGlobalFiltersSync().find((filter) => filter.name === config.filter);
+let issues = donedone.getIssuesByFilterSync(filter.id).issues;
+
+console.log(issues.length);
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket){
+    console.log("a user connected");
+    socket.emit("data", data);
+
+    
+});
+
+http.listen(3000, function(){
+    console.log("listening on *:3000");
 });
