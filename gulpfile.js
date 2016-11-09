428c00a95ceda75263d7082f9473d79a507546ec
jazcash <jaspercashmore@gmail.com>
2016-11-09 16:58:27 +0000

    Progress!

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require("gulp");
+var nodemon = require("gulp-nodemon");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+var browserSync = require('browser-sync').create();
+
+gulp.task("sass", function () {
+	return gulp.src("./public/styles.scss")
+	.pipe(plumber())
+	.pipe(sass())
+	.pipe(gulp.dest("./public/build"))
+	.pipe(browserSync.stream());
+});
+
+gulp.task("scripts", function(){
+	return gulp.src("./public/scripts.js")
+	.pipe(plumber())
+	.pipe(gulp.dest("./public/build"))
+	.pipe(browserSync.stream());
+});
+
+gulp.task("watch", function() {
+	gulp.watch("./public/styles.scss", ["sass"]);
+	gulp.watch("./public/scripts.js", ["scripts"]);
+	gulp.watch("./index.js").on("change", browserSync.reload);
+});
+
+gulp.task("browser-sync", ["nodemon"], function() {
+    browserSync.init({
+        proxy: "http://localhost:3002",
+        port: 7002
+    });
+});
+
+gulp.task("nodemon", function (cb) {
+	var started = false;
+	return nodemon({
+		script: "index.js"
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true;
+		}
+	});
+});
+
+gulp.task("default", [
+	"sass",
+	"scripts",
+	"browser-sync",
+	"watch"
+]);
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const _ = require("underscore");
 const Donedone = require("donedone.js");
 const config = require("./config");
 
@@ -11,27 +13,58 @@ let donedone = new Donedone({
 });
 
 let filter = donedone.getGlobalFiltersSync().find((filter) => filter.name === config.filter);
-let issues = donedone.getIssuesByFilterSync(filter.id).issues;
+let _issues = donedone.getIssuesByFilterSync(filter.id).issues;
 
-setInterval(function(){
-    issues = donedone.getIssuesByFilterSync(filter.id).issues;
-    io.emit("issues", issues);
-}, 5000);
+let issues = {
+    backlog: [],
+    unassigned: [],
+    inprogress: [],
+    withclient: [],
+    golive: []
+};
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-});
+console.log(_issues.length);
 
-io.on("connection", function(socket){
-    console.log("A client connected");
+for (let i=0; i<_issues.length; ++i){
+    let issue = _issues[i];
+    switch(issue.status.name){
+        case "Pushed Back": 
+            issues.backlog.push(issue);
+            break;
+        case "In Progress":
+            issues.inprogress.push(issue);
+            break;
+        case "Ready for Retest":
+            issues.withclient.push(issue);
+            break;
+    }
+}
 
-    socket.emit("issues", issues);
+console.log(_.sortBy(issues.withclient, function(item){
+    return item.fixer.name;
+}).map((x) => x.fixer.name));
+
+// setInterval(function(){
+//     issues = donedone.getIssuesByFilterSync(filter.id).issues;
+//     io.emit("issues", issues);
+// }, 5000);
+
+// app.use(express.static('public'));
+
+// app.get("/", function(req, res){
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+// io.on("connection", function(socket){
+//     console.log("A client connected");
+
+//     socket.emit("issues", issues);
     
-    socket.on("disconnect", function(){
-        console.log("A client disconnected");
-    });
-});
+//     socket.on("disconnect", function(){
+//         console.log("A client disconnected");
+//     });
+// });
 
-http.listen(3000, function(){
-    console.log("listening on *:3000");
-});
+// http.listen(3002, function(){
+//     console.log("listening on *:3002");
+// });
